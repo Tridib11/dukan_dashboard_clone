80c3df6cc4d8f366a57dd678cb282cef09edd2f9
Tridib11 <tridibghosh12345@gmail.com>
2024-03-14 04:17:43 +0530

    Files added

--- src/component/RevenueCard.jsx
+++ src/component/RevenueCard.jsx
@@ -6,7 +6,7 @@ export const RevenueCard=({
     amount
 })=>{
   return (
-    <div className='bg-white rounded shadow-md p-10'>
+    <div className='bg-white rounded shadow-md p-2'>
         <div className='text-gray-700'>
             {title}
             ?
@@ -15,9 +15,13 @@ export const RevenueCard=({
             <div>
              ₹ {amount}
             </div>
-            {orderCount ? <div>
-                {orderCount} orders {">"}
+            {orderCount ? <div className='flex'>
+                {orderCount} orders
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+            </svg>
             </div>:null}
+
         </div>     
     </div>
   )
