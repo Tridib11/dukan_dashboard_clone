df6dfa3ca0aafe99e0f0f1bb505184134d05061a
Tridib11 <tridibghosh12345@gmail.com>
2024-03-14 15:30:07 +0530

    File added

---
+++ src/stories/components/RevenueCard.stories.jsx
@@ -0,0 +1,18 @@
+
+
+import { RevenueCard } from '../../component/RevenueCard';
+
+const meta= {
+  component: RevenueCard,
+};
+
+export default meta;
+
+
+export const Primary = {
+  args: {
+    orderCount: "123",
+    title:"Asd",
+    amount:"10"
+  },
+};
